94b20d29535b9f2b71335324866de374d707a6c4
Frederik Goovaerts <info@frederik.dev>
2021-03-31 20:27:29 +0200

    Add basic express with OIDC auth

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log("Hello world");
+import express from "express";
+import { auth, requiresAuth } from "express-openid-connect";
+
+const app = express();
+const port = 8080;
+
+app.use(
+    auth({
+        issuerBaseURL: process.env.OIDC_ISSUER,
+        baseURL: process.env.OIDC_BASE_URL,
+        clientID: process.env.OIDC_CLIENT_ID,
+        secret: process.env.OIDC_SECRET,
+        authRequired: false
+    })
+);
+
+app.get("/profile", requiresAuth(), (req, res) => {
+    console.log((req as any).oidc.user);
+    res.send(`Hello ${(req as any).oidc.user.name}`);
+});
+
+app.get("/", (req, res) => {
+    res.send(`Hello world`);
+});
+
+app.listen(port, () => {
+    console.log(
+        `Stackoverflow Light backend started at http://localhost:${port}`
+    );
+});
