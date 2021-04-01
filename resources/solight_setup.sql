4975410850755778730ad95c82ce359799ff4ea2
Frederik Goovaerts <info@frederik.dev>
2021-04-01 16:14:15 +0200

    Add database fields for metrics

--- resources/solight_setup.sql
+++ resources/solight_setup.sql
@@ -2,15 +2,23 @@ CREATE TABLE IF NOT EXISTS question (
   id UUID NOT NULL,
   content TEXT NOT NULL,
   score INT NOT NULL,
+  ts TIMESTAMP DEFAULT NOW(),
   PRIMARY KEY (id)
 );
-
 
 CREATE TABLE IF NOT EXISTS answer (
   id UUID NOT NULL,
   question_id UUID NOT NULL,
   content TEXT NOT NULL,
+  ts TIMESTAMP DEFAULT NOW(),
+  PRIMARY KEY (id),
   CONSTRAINT fk_question
     FOREIGN KEY(question_id) 
       REFERENCES question(id)
+);
+
+CREATE TABLE IF NOT EXISTS souser (
+  id UUID NOT NULL,
+  sub TEXT NOT NULL,
+  PRIMARY KEY (id)
 );
