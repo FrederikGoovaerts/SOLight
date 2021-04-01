1556dcdfbf1fda754c2e21e46c622ac37f645ee0
Frederik Goovaerts <info@frederik.dev>
2021-04-01 17:33:03 +0200

    Simplify authentication with bearer tokens

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,12 +1,10 @@
 import express from "express";
-import { auth } from "express-openid-connect";
 import questionRouter from "./questionRouter";
 import metadataRouter from "./metadataRouter";
 
 const port = 8080;
 const app = express();
 
-app.use(auth({ authRequired: false }));
 app.use(express.json());
 app.use(questionRouter);
 app.use(metadataRouter);
--- src/api/questionRouter.ts
+++ src/api/questionRouter.ts
@@ -1,7 +1,11 @@
 import express from "express";
-import { OpenidRequest, requiresAuth } from "express-openid-connect";
+import jwt from "express-jwt";
 import { db } from "../db/DatabaseWrapper";
 
+const jwtMiddleware = jwt({
+    secret: process.env.JWT_SECRET ?? "",
+    algorithms: [process.env.JWT_ALGO ?? ""]
+});
 const router = express.Router();
 
 router.get("/questions", async (req, res) => {
@@ -15,7 +19,7 @@ router.get("/questions/:id", async (req, res) => {
     res.send(question);
 });
 
-router.post("/questions", requiresAuth(), async (req, res) => {
+router.post("/questions", jwtMiddleware, async (req, res) => {
     if (req.body?.content == undefined) {
         res.sendStatus(400);
         return;
@@ -23,13 +27,11 @@ router.post("/questions", requiresAuth(), async (req, res) => {
     await db.createQuestion(req.body.content);
     res.sendStatus(200);
 
-    const { oidc } = req as OpenidRequest;
-    if (oidc.user && (oidc.user as Record<string, string>).sub) {
-        void db.addActiveUser((oidc.user as Record<string, string>).sub);
-    }
+    const { sub } = req.user as { sub: string };
+    void db.addActiveUser(sub);
 });
 
-router.post("/questions/:id/answers", requiresAuth(), async (req, res) => {
+router.post("/questions/:id/answers", jwtMiddleware, async (req, res) => {
     const questionId = req.params.id;
     if (req.body?.content === undefined) {
         res.sendStatus(400);
@@ -38,10 +40,8 @@ router.post("/questions/:id/answers", requiresAuth(), async (req, res) => {
     await db.createAnswer(questionId, req.body.content);
     res.sendStatus(200);
 
-    const { oidc } = req as OpenidRequest;
-    if (oidc.user && (oidc.user as Record<string, string>).sub) {
-        void db.addActiveUser((oidc.user as Record<string, string>).sub);
-    }
+    const { sub } = req.user as { sub: string };
+    void db.addActiveUser(sub);
 });
 
 router.post("/questions/:id/upvote", async (req, res) => {
